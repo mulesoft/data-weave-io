71f0527d6041b6c27be61488fe60fe96f5ce94fa
mlischetti <lischettim@gmail.com>
2023-06-08 15:28:30 -0300

    Add comment

--- http-netty-module/src/test/scala/org/mule/weave/io/http/netty/HttpNettyTest.scala
+++ http-netty-module/src/test/scala/org/mule/weave/io/http/netty/HttpNettyTest.scala
@@ -5,6 +5,7 @@ import org.mule.weave.v2.helper.AbstractEngineTest
 class HttpNettyTest extends AbstractEngineTest {
 
   override def ignoreTests(): Array[String] = {
+    // TODO: Flaky test, see W-13564473
     Array("GET_POST")
   }
 }
