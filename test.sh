596995ee42e2553e23b0cb9e5f3f45c0185cddc1
Mariano Lischetti <lischettim@gmail.com>
2023-12-26 15:56:31 -0300

    W-14718662: Add build logs (#72)

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 set -x
 
 if [ $# -ne 1 ]
@@ -8,5 +9,24 @@ then
     exit 1
 fi
 
-./gradlew -Pmaven.settings.location=$1 test
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "no java"
+fi
 
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" < "1.9" ]]; then
+        ./gradlew -Pmaven.settings.location=$1 test
+    else
+        ./gradlew -Pmaven.settings.location=$1 clean
+        ./gradlew -Pmaven.settings.location=$1 build -x test
+        ./gradlew -Pmaven.settings.location=$1 test
+    fi
+fi
