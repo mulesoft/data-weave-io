dc89b0f76122ea18f3fdc5ea552b93327cf51f47
mlischetti <lischettim@gmail.com>
2024-02-22 15:33:03 -0300

    W-15084038: Fix deploy.sh and set upstreampProject to support/2.6.x

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,19 @@ then
     exit 1
 fi
 
-source gradle.properties
+# source gradle.properties
 
-dir=$PWD
-echo $dir
-echo "Listing file-module classes ..."
-ls -l -R file-module/build/classes
+# dir=$PWD
+# echo $dir
+# echo "Listing file-module classes ..."
+# ls -l -R file-module/build/classes
 
-echo "Extracting file-module JAR content ..."
-mkdir -p file-module/build/extracted-jar
-cd file-module/build/extracted-jar
-jar -xfv ../libs/file-module-${projectVersion}.jar
+# echo "Extracting file-module JAR content ..."
+# mkdir -p file-module/build/extracted-jar
+# cd file-module/build/extracted-jar
+# jar -xfv ../libs/file-module-${projectVersion}.jar
 
-cd $dir
+# cd $dir
 if [[ "$BRANCH_NAME" =~ ^(master$|support/.|release/.) ]]
 then
     ./gradlew -Pmaven.settings.location=$1 clean
