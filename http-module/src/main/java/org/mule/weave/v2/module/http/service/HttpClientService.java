9b4f093d6e96dab5faf409bb44446eb62f0c2058
mlischetti <lischettim@gmail.com>
2024-06-12 10:47:39 -0300

    More docs

--- http-module/src/main/java/org/mule/weave/v2/module/http/service/HttpClientService.java
+++ http-module/src/main/java/org/mule/weave/v2/module/http/service/HttpClientService.java
@@ -3,7 +3,11 @@ package org.mule.weave.v2.module.http.service;
 /**
  * Factory object for {@link HttpClient} instances. It creates a {@link HttpClient} instance
  * for every {@link HttpClientConfiguration}.
+ * <p>
  * The {@link HttpClient} was designed to be stateless and reusable for the same {@link HttpClientConfiguration}.
+ * <p>
+ * Ideally, the {@link HttpClient} should not be related to any Cookie Store strategy to be able to send stateless requests.
+ * <p>
  * If you need to reuse {@link HttpClient} instances for a set of requests consider implementing a cache strategy based
  * on {@link HttpClientConfiguration}.
  */
