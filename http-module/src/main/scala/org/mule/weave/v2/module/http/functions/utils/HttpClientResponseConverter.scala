810ba994e4101c9b26e7743be411bd410200f537
Mariano Lischetti <lischettim@gmail.com>
2024-08-08 14:02:44 -0300

    W-16439420: Validate content-length and the status code to send HTTP response body (#126)

--- http-module/src/main/scala/org/mule/weave/v2/module/http/functions/utils/HttpClientResponseConverter.scala
+++ http-module/src/main/scala/org/mule/weave/v2/module/http/functions/utils/HttpClientResponseConverter.scala
@@ -21,10 +21,16 @@ import org.mule.weave.v2.module.http.service.HttpClientHeaders
 import org.mule.weave.v2.module.http.service.HttpClientResponse
 import org.mule.weave.v2.module.reader.SourceProvider
 
+import java.io.InputStream
+import java.lang.Long.parseLong
 import java.net.HttpCookie
 import scala.collection.JavaConverters._
 
 class HttpClientResponseConverter(response: HttpClientResponse, stopWatch: StopWatch) {
+  private val CONTENT_LENGTH_HEADER = "Content-Length"
+  private val NO_CONTENT_STATUS_CODE = 204
+  private val NOT_MODIFIED_STATUS_CODE = 304
+  private val RESET_CONTENT_STATUS_CODE = 205
 
   def convert()(implicit ctx: EvaluationContext): ObjectValue = {
 
@@ -43,8 +49,7 @@ class HttpClientResponseConverter(response: HttpClientResponse, stopWatch: StopW
 
     // body?
     response.getBody.ifPresent(body => {
-      val sourceProvider = SourceProvider(SeekableStream(body))
-      builder.addPair(BODY, BinaryValue(sourceProvider.asInputStream))
+      addBody(body, response.getStatus, response.getHeaders, builder)
     })
 
     // cookies
@@ -89,6 +94,50 @@ class HttpClientResponseConverter(response: HttpClientResponse, stopWatch: StopW
       Seq.empty[KeyValuePair]
     }
     ObjectValue(entries.toArray)
+  }
+
+  private def addBody(body: InputStream, statusCode: Int, headers: HttpClientHeaders, builder: ObjectValueBuilder)(implicit ctx: EvaluationContext): Unit = {
+    var addBodyField = true
+    val maybeContentLength = extractContentLength(headers)
+    if (maybeContentLength.isDefined) {
+      // Validate content-length header
+      val contentLength = maybeContentLength.get
+      if (contentLength <= 0) {
+        addBodyField = false
+        ctx.serviceManager.loggingService.logDebug(s"Ignoring HTTP response body field because $CONTENT_LENGTH_HEADER header value is $contentLength")
+      }
+    } else {
+      // Validate status code
+      if (NO_CONTENT_STATUS_CODE == statusCode || NOT_MODIFIED_STATUS_CODE == statusCode || RESET_CONTENT_STATUS_CODE == statusCode) {
+        addBodyField = false
+        ctx.serviceManager.loggingService.logDebug(s"Ignoring HTTP response body field because the status: $statusCode does not support body")
+      }
+    }
+
+    if (addBodyField) {
+      val sourceProvider = SourceProvider(SeekableStream(body))
+      builder.addPair(BODY, BinaryValue(sourceProvider.asInputStream))
+    }
+  }
+
+  private def extractContentLength(headers: HttpClientHeaders): Option[Long] = {
+    if (headers != null) {
+      val contentLengthHeaderValues = headers.getHeaderValue(CONTENT_LENGTH_HEADER)
+      if (contentLengthHeaderValues.isPresent) {
+        var contentLength: Option[Long] = None
+        try {
+          contentLength = Some(parseLong(contentLengthHeaderValues.get()))
+        } catch {
+          case _: NumberFormatException =>
+          // Nothing to do
+        }
+        contentLength
+      } else {
+        None
+      }
+    } else {
+      None
+    }
   }
 }
 
