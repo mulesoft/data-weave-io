146b90cf7b4add72e0865b35295ba65393e869a7
mlischetti <lischettim@gmail.com>
2024-11-11 10:28:45 -0300

    Revert "W-17157731: Allow enabling the system properties http proxy setup (#143)"
    This reverts commit 3f39ec6a4ec606c1a1666615951d9106cba9a69a.

--- http-netty-module/src/main/scala/org/mule/weave/v2/module/http/netty/NettyHttpClientFactory.scala
+++ http-netty-module/src/main/scala/org/mule/weave/v2/module/http/netty/NettyHttpClientFactory.scala
@@ -21,12 +21,6 @@ object NettyHttpClientFactory {
     // Using empty cookie store
     asyncConfig.setCookieStore(EmptyCookieStore())
 
-    //The weave.netty.client.honorProxyProperties allows disabling the system properties configuration for proxy setup.
-    val skipSetUseProxyProperties = sys.props.get("weave.netty.client.honorProxyProperties") exists (_ equalsIgnoreCase "false")
-    if (!skipSetUseProxyProperties) {
-      asyncConfig.setUseProxyProperties(true)
-    }
-
     asyncConfig.setCompressionEnforced(configuration.isCompressionHeader)
 
     if (!configuration.isDecompress) {
