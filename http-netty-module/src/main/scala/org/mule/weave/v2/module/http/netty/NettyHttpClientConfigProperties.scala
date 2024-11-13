2371c6ac12439e069bc4e9fb06447c5c177ca64e
mlischetti <lischettim@gmail.com>
2024-11-13 17:18:37 -0300

    W-17157731: Allow disabling the system properties http proxy setup

--- http-netty-module/src/main/scala/org/mule/weave/v2/module/http/netty/NettyHttpClientConfigProperties.scala
+++ http-netty-module/src/main/scala/org/mule/weave/v2/module/http/netty/NettyHttpClientConfigProperties.scala
@@ -1,5 +1,13 @@
 package org.mule.weave.v2.module.http.netty
 
-object NettyHttpClientConfiguration {
+import org.mule.weave.v2.model.service.RuntimeSettings
 
+object NettyHttpClientConfigProperties {
+
+  // prefix value = "com.mulesoft.dw.netty.client"
+  private val NETTY_CLIENT_SETTINGS_PREFIX = RuntimeSettings.prop("netty.client")
+
+  private val HONOR_PROXY_PROPERTIES_SETTING = s"$NETTY_CLIENT_SETTINGS_PREFIX.honor_proxy_properties"
+
+  lazy val HONOR_PROXY_PROPERTIES: Boolean = System.getProperty(HONOR_PROXY_PROPERTIES_SETTING, "true").toBoolean
 }
