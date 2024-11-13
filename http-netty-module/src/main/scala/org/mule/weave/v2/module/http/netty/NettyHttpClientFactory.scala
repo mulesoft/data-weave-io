a7fa2413b5053937b377554389ec0f69bc32d269
Mariano Lischetti <lischettim@gmail.com>
2024-11-13 17:39:43 -0300

    W-17157731: Allow disabling the system properties http proxy setup (#145)
    (cherry picked from commit 588175ec6807eeb2376c64b8d711b74e1e83f197)

--- http-netty-module/src/main/scala/org/mule/weave/v2/module/http/netty/NettyHttpClientFactory.scala
+++ http-netty-module/src/main/scala/org/mule/weave/v2/module/http/netty/NettyHttpClientFactory.scala
@@ -21,6 +21,14 @@ object NettyHttpClientFactory {
     // Using empty cookie store
     asyncConfig.setCookieStore(EmptyCookieStore())
 
+    // "com.mulesoft.dw.netty.client.honor_proxy_properties" allows disabling the system properties configuration
+    // for proxy setup.
+    if (!NettyHttpClientConfigProperties.HONOR_PROXY_PROPERTIES) {
+      asyncConfig.setUseProxyProperties(false)
+    } else {
+      asyncConfig.setUseProxyProperties(true)
+    }
+
     asyncConfig.setCompressionEnforced(configuration.isCompressionHeader)
 
     if (!configuration.isDecompress) {
